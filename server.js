da19fe95aca642ce63b2628d344d7e003929d39b
Tanjim702 <tanjim9870@gmail.com>
2020-12-25 14:47:57 +0600

    Environment configuration complete

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const moongoose = require('mongoose')
@@ -7,9 +8,9 @@ const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
 //Session store
 const store = new MongoDBStore({
-    uri:'mongodb+srv://tanjim:tanjim@cluster0.i5mdg.mongodb.net/tanjim?retryWrites=true&w=majority',
-    collection:'mySessions',
-    expires:1000*60*60*2
+    uri: process.env.DB_URI,
+    collection: 'mySessions',
+    expires: 1000 * 60 * 60 * 2
 });
 //session store
 //Template Engine
@@ -19,24 +20,27 @@ app.set('view engine', '.hbs');
 
 // Import Middlewares
 const setLocals = require('./middlewares/setLocals')
-const {bindUserWithRequest} = require('./middlewares/authMiddleware')
+const { bindUserWithRequest } = require('./middlewares/authMiddleware')
 //Middlewares
 app.use(session({
-    secret:process.env.SECRET_KEY || 'SECRET_KEY',
-    resave:false,
-    saveUninitialized:false,
-    store:store,
+    secret: process.env.SECRET_KEY || 'SECRET_KEY',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
 
 
 }))
-app.use(morgan('dev'))
+
+if (app.get('env').toLowerCase() === 'development') {
+    app.use(morgan('dev'))
+}
 app.use(express.urlencoded({ extended: true }))//Like body parser
 app.use(bindUserWithRequest())
 app.use(setLocals())
 //Public directory
 app.use(express.static('public'))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 
 //Routes
@@ -49,7 +53,7 @@ app.get('/', (req, res) => {
     })
 })
 //Routes
-moongoose.connect('mongodb+srv://tanjim:tanjim@cluster0.i5mdg.mongodb.net/tanjim?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+moongoose.connect(`${process.env.DB_URI}`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log(`Connected to datebase!`)
         app.listen(PORT, () => {
